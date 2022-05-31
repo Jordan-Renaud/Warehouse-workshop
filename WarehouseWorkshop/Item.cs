be69b59d953200ff6be28c798b7d371189943ecf
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-31 13:45:49 +0100

    Create item class

---
+++ WarehouseWorkshop/Item.cs
@@ -0,0 +1,19 @@
+namespace WarehouseWorkshop
+{
+    public class Item
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+
+        public Item(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void LogItemDetails()
+        {
+            Console.WriteLine($"Item name: {Name}, Price: £{Price}");
+        }
+    }
+}
--- WarehouseWorkshop/Program.cs
+++ WarehouseWorkshop/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            List<Item> items = new List<Item> { new Item("Banana", 1.5M), new Item("Apple", 1.0M), new Item("Carrot", 1.75M) };
+
+            foreach (var item in items)
+            {
+                item.LogItemDetails();
+            }
         }
     }
 }
